cc37b7e9a0d5323c66144265ea801ff9c17e70b2
Edgar Efrain Campa Arroyo <ecampa@petrolike.local>
2021-06-21 21:35:47 -0500

    Tarea ejercicio 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -146,6 +146,19 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if(num1 === 0 || num2 === 0 || num3 === 0){
+    return "Error";
+  }
+  else if(num1 < 0 || num2 < 0 || num3 < 0){
+   return "Hay negativos"
+  }
+  else if (num3 > num1 && num3 > num2){
+    return(num3 + 1)
+  }
+  else if (num1 > num2 && num1 > num3 && num1 > 0){
+    return "Número 1 es mayor y positivo"
+  }
+  return false;
 }
 
 function esPrimo(numero) {
@@ -154,25 +167,45 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero === 0 || numero === 1)return false;
+  for(var i = 2; i < numero; i++){
+    if (numero % i === 0){
+      return false;
+  }
+ }
+ return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+if(valor) {
+  return"Soy verdadero"
+}
+return"Soy falso"
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if(numero > 99 && numero < 1000){
+    return true;
+  }
+  return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var resultado = numero;
+  var i = 0;
+  do {
+    resultado = resultado + 5;
+    i++
+  }while(i < 8)
+  return resultado;
 }
 
 
